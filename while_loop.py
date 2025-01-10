ac1fa0f0e6e01573d75052840d0fdbae1a27d94a
Arun436-Debug <arunkandukuri94@gmail.com>
2025-01-10 16:55:43 +0530

    one decorator code  was added to my_first_branch

--- while_loop.py
+++ while_loop.py
@@ -3,3 +3,16 @@ i = 1
 while i<=num:
     print(i)
     i = i+1
+    
+#adding one decorator
+
+def real_fun(f):
+    def inner_fun(s):
+        return s+" how are you ?"
+    return inner_fun
+@real_fun
+def fun(s):
+    return s
+s = "hii bro!"
+res = fun(s)
+print(res)    
